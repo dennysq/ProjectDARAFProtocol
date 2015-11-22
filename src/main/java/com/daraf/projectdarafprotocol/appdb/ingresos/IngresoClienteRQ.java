b19d34801539b899a090d7fef5e4cc7cd42df27f
RAUL <RAUL@david-PC>
2015-11-22 08:19:31 -0500

    funcion

--- src/main/java/com/daraf/projectdarafprotocol/appdb/ingresos/IngresoClienteRQ.java
+++ src/main/java/com/daraf/projectdarafprotocol/appdb/ingresos/IngresoClienteRQ.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.daraf.projectdarafprotocol.ingresos;
+package com.daraf.projectdarafprotocol.appdb.ingresos;
 
 /**
  *
  * @author Alejandra Ponce
  */
-public class IngresoFacturaRQ {
+public class IngresoClienteRQ {
     
 }

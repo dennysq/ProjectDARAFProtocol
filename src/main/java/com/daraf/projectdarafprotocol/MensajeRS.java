2f15c46d50f95a90bd70afd97140fd72ffb4d6a0
aledennis93 <aledennis.93@gmail.com>
2015-11-22 07:59:37 -0500

    MensajeRS

--- src/main/java/com/daraf/projectdarafprotocol/MensajeRS.java
+++ src/main/java/com/daraf/projectdarafprotocol/MensajeRS.java
@@ -5,10 +5,74 @@
  */
 package com.daraf.projectdarafprotocol;
 
+import com.daraf.projectdarafprotocol.consultas.ConsultaClienteRS;
+import com.daraf.projectdarafprotocol.consultas.ConsultaFacturaRS;
+import com.daraf.projectdarafprotocol.ingresos.IngresoClienteRS;
+import com.daraf.projectdarafprotocol.ingresos.IngresoFacturaRS;
+import com.daraf.projectdarafprotocol.seguridades.AutenticacionClienteRS;
+
 /**
  *
  * @author Alejandra Ponce
  */
-public class MensajeRS {
-    
+public class MensajeRS extends Mensaje {
+	public MensajeRS() {
+	}
+
+	public MensajeRS(String originador, String idMensaje) {
+		this.cabecera = new Cabecera(Mensaje.TIPO_MENSAJE_RESPONSE, originador, idMensaje);
+	}
+
+	@Override
+	public boolean build(String input) {
+		boolean result = true;
+		if (validate(input)) {
+			this.cabecera = new Cabecera();
+
+			if (this.cabecera.build(input.substring(0, Cabecera.HEADER_LENGTH))) {
+				String cuerpo = input.substring(Cabecera.HEADER_LENGTH);
+
+				if (this.cabecera.getTipoMensaje().equals(Mensaje.TIPO_MENSAJE_RESPONSE)) {
+					switch (this.cabecera.getIdMensaje()) {
+					case ID_MENSAJE_AUTENTICACIONCLIENTE:
+						AutenticacionClienteRS autenticacionClienteRS = new AutenticacionClienteRS();
+						autenticacionClienteRS.build(cuerpo);
+						this.cuerpo = autenticacionClienteRS;
+						break;
+					case ID_MENSAJE_CONSULTACLIENTE:
+						ConsultaClienteRS consultaClienteRS = new ConsultaClienteRS();
+						consultaClienteRS.build(cuerpo);
+						this.cuerpo = consultaClienteRS;
+						break;
+					case ID_MENSAJE_CONSULTAFACTURA:
+						ConsultaFacturaRS consultaFacturaRS = new ConsultaFacturaRS();
+						consultaFacturaRS.build(cuerpo);
+						this.cuerpo = consultaFacturaRS;
+						break;
+					case ID_MENSAJE_INGRESOCLIENTE:
+						IngresoClienteRS ingresoClienteRS = new IngresoClienteRS();
+						ingresoClienteRS.build(cuerpo);
+						this.cuerpo = ingresoClienteRS;
+						break;
+					case ID_MENSAJE_INGRESOFACTURA:
+						IngresoFacturaRS ingresoFacturaRS = new IngresoFacturaRS();
+						ingresoFacturaRS.build(cuerpo);
+						this.cuerpo = ingresoFacturaRS;
+						break;
+					default:
+						result = false;
+					}
+				} else {
+					result = false;
+				}
+			} else {
+				result = false;
+			}
+		} else {
+			result = false;
+		}
+		return result;
+
+	}
+
 }

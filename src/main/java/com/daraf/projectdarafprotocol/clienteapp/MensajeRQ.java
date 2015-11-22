79a9a86294d2c3ebaa37f615457896e9202679df
aledennis93 <aledennis.93@gmail.com>
2015-11-22 09:10:05 -0500

    Creación paqueet protocolo cliente - app

---
+++ src/main/java/com/daraf/projectdarafprotocol/clienteapp/MensajeRQ.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daraf.projectdarafprotocol.clienteapp;
+
+import com.daraf.projectdarafprotocol.Cabecera;
+import com.daraf.projectdarafprotocol.Mensaje;
+import com.daraf.projectdarafprotocol.appdb.consultas.ConsultaClienteRQ;
+import com.daraf.projectdarafprotocol.appdb.consultas.ConsultaFacturaRQ;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoClienteRQ;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoFacturaRQ;
+import com.daraf.projectdarafprotocol.appdb.seguridades.AutenticacionClienteRQ;
+
+/**
+ *
+ * @author Alejandra Ponce
+ */
+public class MensajeRQ extends Mensaje {
+	public MensajeRQ() {
+	}
+
+	public MensajeRQ(String originador, String idMensaje) {
+		this.cabecera = new Cabecera(Mensaje.TIPO_MENSAJE_REQUEST, originador, idMensaje);
+	}
+
+	@Override
+	public boolean build(String input) {
+		boolean result = true;
+		if (validate(input)) {
+			this.cabecera = new Cabecera();
+			// Prueba repositorio GIT
+			if (this.cabecera.build(input.substring(0, Cabecera.HEADER_LENGTH))) {
+				// se obtiene el resto del mensaje que seria el cuerpo
+				String cuerpo = input.substring(Cabecera.HEADER_LENGTH);
+				if (this.cabecera.getTipoMensaje().equals(Mensaje.TIPO_MENSAJE_REQUEST)) {
+					switch (this.cabecera.getIdMensaje()) {
+					case ID_MENSAJE_AUTENTICACIONCLIENTE:
+						AutenticacionClienteRQ autenticacionClienteRQ = new AutenticacionClienteRQ();
+                                                autenticacionClienteRQ.build(cuerpo);
+						this.cuerpo = autenticacionClienteRQ;
+						break;
+					case ID_MENSAJE_CONSULTACLIENTE:
+						ConsultaClienteRQ consultaClienteRQ = new ConsultaClienteRQ();
+						consultaClienteRQ.build(cuerpo);
+						this.cuerpo = consultaClienteRQ;
+						break;
+					case ID_MENSAJE_CONSULTAFACTURA:
+						ConsultaFacturaRQ consultaFacturaRQ = new ConsultaFacturaRQ();
+						consultaFacturaRQ.build(cuerpo);
+						this.cuerpo = consultaFacturaRQ;
+						break;
+					case ID_MENSAJE_INGRESOCLIENTE:
+						IngresoClienteRQ ingresoClienteRQ = new IngresoClienteRQ();
+						ingresoClienteRQ.build(cuerpo);
+						this.cuerpo = ingresoClienteRQ;
+						break;
+					case ID_MENSAJE_INGRESOFACTURA:
+						IngresoFacturaRQ ingresoFacturaRQ = new IngresoFacturaRQ();
+						ingresoFacturaRQ.build(cuerpo);
+						this.cuerpo = ingresoFacturaRQ;
+						break;
+					default:
+						result = false;
+					}
+				} else {
+					result = false;
+				}
+
+			} else {
+				result = false;
+			}
+		} else {
+			result = false;
+		}
+		return result;
+
+	}
+
+}
+
---
+++ src/main/java/com/daraf/projectdarafprotocol/clienteapp/MensajeRS.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daraf.projectdarafprotocol.clienteapp;
+
+import com.daraf.projectdarafprotocol.Cabecera;
+import com.daraf.projectdarafprotocol.Mensaje;
+import com.daraf.projectdarafprotocol.appdb.consultas.ConsultaClienteRS;
+import com.daraf.projectdarafprotocol.appdb.consultas.ConsultaFacturaRS;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoClienteRS;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoFacturaRS;
+import com.daraf.projectdarafprotocol.appdb.seguridades.AutenticacionClienteRS;
+
+/**
+ *
+ * @author Alejandra Ponce
+ */
+public class MensajeRS extends Mensaje {
+	public MensajeRS() {
+	}
+
+	public MensajeRS(String originador, String idMensaje) {
+		this.cabecera = new Cabecera(Mensaje.TIPO_MENSAJE_RESPONSE, originador, idMensaje);
+	}
+
+	@Override
+	public boolean build(String input) {
+		boolean result = true;
+		if (validate(input)) {
+			this.cabecera = new Cabecera();
+
+			if (this.cabecera.build(input.substring(0, Cabecera.HEADER_LENGTH))) {
+				String cuerpo = input.substring(Cabecera.HEADER_LENGTH);
+
+				if (this.cabecera.getTipoMensaje().equals(Mensaje.TIPO_MENSAJE_RESPONSE)) {
+					switch (this.cabecera.getIdMensaje()) {
+					case ID_MENSAJE_AUTENTICACIONCLIENTE:
+						AutenticacionClienteRS autenticacionClienteRS = new AutenticacionClienteRS();
+						autenticacionClienteRS.build(cuerpo);
+						this.cuerpo = autenticacionClienteRS;
+						break;
+					case ID_MENSAJE_CONSULTACLIENTE:
+						ConsultaClienteRS consultaClienteRS = new ConsultaClienteRS();
+						consultaClienteRS.build(cuerpo);
+						this.cuerpo = consultaClienteRS;
+						break;
+					case ID_MENSAJE_CONSULTAFACTURA:
+						ConsultaFacturaRS consultaFacturaRS = new ConsultaFacturaRS();
+						consultaFacturaRS.build(cuerpo);
+						this.cuerpo = consultaFacturaRS;
+						break;
+					case ID_MENSAJE_INGRESOCLIENTE:
+						IngresoClienteRS ingresoClienteRS = new IngresoClienteRS();
+						ingresoClienteRS.build(cuerpo);
+						this.cuerpo = ingresoClienteRS;
+						break;
+					case ID_MENSAJE_INGRESOFACTURA:
+						IngresoFacturaRS ingresoFacturaRS = new IngresoFacturaRS();
+						ingresoFacturaRS.build(cuerpo);
+						this.cuerpo = ingresoFacturaRS;
+						break;
+					default:
+						result = false;
+					}
+				} else {
+					result = false;
+				}
+			} else {
+				result = false;
+			}
+		} else {
+			result = false;
+		}
+		return result;
+
+	}
+
+}

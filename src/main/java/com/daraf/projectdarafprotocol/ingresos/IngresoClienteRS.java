e28cc638955d0c0b8b165e2660ca572650545b0d
aledennis93 <aledennis.93@gmail.com>
2015-11-22 07:29:02 -0500

    Clases IngresoFacturaRQ, IngresoFacturaRS, IngresoClienteRQ, IngresoClienteRS, ConsultaClienteRQ. ConsultaClienteRS, ConsultaFacturaRQ, ConsultaFacturaRS

---
+++ src/main/java/com/daraf/projectdarafprotocol/ingresos/IngresoClienteRS.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daraf.projectdarafprotocol.ingresos;
+
+/**
+ *
+ * @author Alejandra Ponce
+ */
+public class IngresoClienteRS {
+    
+}

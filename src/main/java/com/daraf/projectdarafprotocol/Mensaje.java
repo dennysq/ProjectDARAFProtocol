74c90c437d301dc11c53cc3a35a85c489f5d1315
aledennis93 <aledennis.93@gmail.com>
2015-11-22 01:27:17 -0500

    Clase Mensaje

---
+++ src/main/java/com/daraf/projectdarafprotocol/Mensaje.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daraf.projectdarafprotocol;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ *
+ * @author Alejandra Ponce
+ */
+public abstract class Mensaje {
+    
+    protected Cabecera cabecera;
+	protected String Tipo;
+	protected Cuerpo cuerpo;
+	public static final String TIPO_MENSAJE_REQUEST = "RQ";
+	public static final String TIPO_MENSAJE_RESPONSE = "RS";
+	public static final String ID_MENSAJE_INFOCLIENTE = "CLIBUSSIMP";
+	public static final String ID_MENSAJE_CUENTACLIENTE = "CLIBUSCUEN";
+	public static final String ID_MENSAJE_AUTENTICACION = "AUTENTIC01";
+	public static final String ID_MENSAJE_DEPOSITO = "TRADEPOSIT";
+	public static final String ID_MENSAJE_RETIRO = "TRARETIROV";
+
+
+
+	public boolean validateBobyHash() {
+		return DigestUtils.md5Hex(this.cuerpo.asTexto()).equals(cabecera.getVerificacion());
+
+	}
+
+	public void setCuerpo(Cuerpo cuerpo) {
+		this.cuerpo = cuerpo;
+		this.cabecera.setLongitudCuerpo(String.valueOf(this.cuerpo.asTexto().length()));
+		this.cabecera.setVerificacion(DigestUtils.md5Hex(this.cuerpo.asTexto()));
+
+	}
+
+	public Cuerpo getCuerpo() {
+
+		return cuerpo;
+	}
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	public String asTexto() {
+
+		return this.cabecera.asTexto() + this.cuerpo.asTexto();
+
+	}
+
+	public abstract boolean build(String input);
+
+	public boolean validate(String input) {
+
+		return input.length() >= Cabecera.HEADER_LENGTH;
+	}
+
+	public Cabecera getCabecera() {
+		return cabecera;
+	}
+
+	public void setCabecera(Cabecera cabecera) {
+		this.cabecera = cabecera;
+	}
+
+    
+}
